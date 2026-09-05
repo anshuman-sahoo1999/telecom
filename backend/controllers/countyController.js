f4d34d46ad47c287e5e531bfc44c5211a112e651
Rituparna07 <106649297+Rituparna07@users.noreply.github.com>
2026-09-05 15:30:49 +0530

    Update countyController.js

--- backend/controllers/countyController.js
+++ backend/controllers/countyController.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
+const path = require("path");
+
+const COUNTIES_PATH = path.join(__dirname, "..", "counties.json");
 
 const cleanName = (name) =>
   name.split(",")[0].replace(/county/i, "").trim();
 
 // ALL counties
 const getCounties = (req, res) => {
-  const data = JSON.parse(fs.readFileSync("./counties.json"));
+  const data = JSON.parse(fs.readFileSync(COUNTIES_PATH));
 
   res.json(data.map(i => cleanName(i.GeographicAreaName)));
 };
@@ -14,7 +17,7 @@ const getCounties = (req, res) => {
 const getByState = (req, res) => {
   const state = req.params.state.toLowerCase();
 
-  const data = JSON.parse(fs.readFileSync("./counties.json"));
+  const data = JSON.parse(fs.readFileSync(COUNTIES_PATH));
 
   const filtered = data
     .filter(i =>
